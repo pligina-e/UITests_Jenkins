39a8a1a15ab2542424b67ce00e497c91b0fa6a38
Eva_Pligina <pligina009@mail.ru>
2023-10-28 03:11:56 +0300

    Обновлен класс "VariousAuthorizationTest"

--- src/test/java/tests/VariousAuthorizationTest.java
+++ src/test/java/tests/VariousAuthorizationTest.java
@@ -5,12 +5,13 @@ import io.qameta.allure.Feature;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Story;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import pages.AuthorizationPage;
 
-import java.util.List;
+import java.util.Objects;
 
 @Epic("Тестирование вкладки авторизации")
 @Feature("Авторизация")
@@ -18,7 +19,7 @@ public class VariousAuthorizationTest extends BaseTest {
     @DataProvider(name = "authorization data")
     public Object[][] createData1() {
         return new Object[][] {
-                { "angular", "pass", "description" },
+                { "angular", "password", "description" },
                 { "Anne", "12345", "description" },
                 { "Tomas", "TomasTomas123", "My name is Tomas" },
         };
@@ -26,7 +27,7 @@ public class VariousAuthorizationTest extends BaseTest {
 
     @Test(description = "Авторизация через меню Resources -> Practice Site2", dataProvider = "authorization data")
     @Severity(SeverityLevel.CRITICAL)
-    @Story("Пользователь вводит некорректные данные в поля Username, Password, Description")
+    @Story("Пользователь вводит корректные/некорректные данные в поля Username, Password, Description")
     public final void authorizationWithDifferentDataTest(String username, String password, String description) {
         SoftAssert softAssert = new SoftAssert();
         AuthorizationPage authorizationPage = new AuthorizationPage(driver);
@@ -34,8 +35,12 @@ public class VariousAuthorizationTest extends BaseTest {
         softAssert.assertEquals(driver.getTitle(), "Protractor and AngularJS practice - sample website", "Открылась не та страница при нажатии Resources -> Practice Site2");
         authorizationPage.pressImageRegistration();
         softAssert.assertTrue(driver.getTitle().contains("Registration"), "Title страницы не содержит \"Registration\". Страница авторизации не открылась");
-        authorizationPage.fillFields(username, password, description);
-        softAssert.assertEquals("Username or password is incorrect", authorizationPage.getErrorText(), "Текст ошибки не совпадает с текстом \"Username or password is incorrect\"");
         softAssert.assertAll();
+        authorizationPage.fillFields(username, password, description);
+        if (driver.getCurrentUrl() == "https://www.way2automation.com/angularjs-protractor/registeration/#/login") {
+            Assert.assertEquals("Username or password is incorrect", authorizationPage.getErrorText(), "Текст ошибки не совпадает с текстом \"Username or password is incorrect\"");
+        } else if (driver.getCurrentUrl() == "https://www.way2automation.com/angularjs-protractor/registeration/#/") {
+            Assert.assertEquals("Home", authorizationPage.getLabelText(), "Что-то пошло не так при авторизации");
+        }
     }
 }
