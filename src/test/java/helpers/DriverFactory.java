c741630971adf5da51f1aec8fd808f71dfe2fce3
Eva_Pligina <pligina009@mail.ru>
2023-11-07 14:25:50 +0300

    Задача U9 в разработке

--- src/test/java/helpers/DriverFactory.java
+++ src/test/java/helpers/DriverFactory.java
@@ -1,7 +1,5 @@
 package helpers;
 
-//import com.opera.core.systems.OperaDriver;
-import com.opera.core.systems.OperaDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -29,26 +27,19 @@ public class DriverFactory {
                 case "chrome":
                     return new ChromeDriver();
                 case "internet explorer":
-                    System.setProperty("webdriver.ie.driver", "C:\\webdrivers\\IEDriverServer.exe");
-                    DesiredCapabilities cap = new DesiredCapabilities();
+                    System.setProperty("webdriver.ie.driver", PropertyProvider.getInstance().getProperty("ie.driver.path"));
+                    InternetExplorerOptions cap = new InternetExplorerOptions();
                     cap.setCapability("nativeEvents", false);
-                    cap.setCapability("unexpectedAlertBehaviour", "accept");
                     cap.setCapability("ignoreProtectedModeSettings", true);
-                    cap.setCapability("disable-popup-blocking", true);
                     cap.setCapability("enablePersistentHover", true);
                     cap.setCapability("ignoreZoomSetting", true);
+                    cap.setCapability("requireWindowFocus", false);
                     cap.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,true);
-                    InternetExplorerOptions options = new InternetExplorerOptions();
-                    options.merge(cap);
-                    return new InternetExplorerDriver(options);
-                case "edge":
+                    return new InternetExplorerDriver(cap);
+                case "MicrosoftEdge":
                     return new EdgeDriver();
                 case "safari":
-                    System.setProperty("webdriver.safari.driver", "C:\\webdrivers\\SafariDriver.safariextz");
                     return new SafariDriver();
-                case "operablink":
-                    System.setProperty("webdriver.opera.driver", "C:\\webdrivers\\operadriver.exe");
-                    return new OperaDriver();
                 default: return null;
             }
         }
