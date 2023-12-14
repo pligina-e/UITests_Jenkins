c41d606bec8a8268198850a2e68fe109af6604a5
pligina-e <82607076+pligina-e@users.noreply.github.com>
2023-12-14 19:03:26 +0300

    Update DriverFactory.java

--- src/test/java/helpers/DriverFactory.java
+++ src/test/java/helpers/DriverFactory.java
@@ -35,13 +35,8 @@ public class DriverFactory {
                     //WebDriverManager.chromedriver().setup();
                     //return new ChromeDriver();
                     ChromeOptions options = new ChromeOptions();
-                    options.addArguments("start-maximized"); // open Browser in maximized mode
+                    options.addArguments("--no-sandbox");
                     options.addArguments("--disable-dev-shm-usage");
-                    options.addArguments("disable-infobars"); // disabling infobars
-                    options.addArguments("--disable-extensions"); // disabling extensions
-                    options.addArguments("--disable-gpu"); // applicable to windows os only
-                    options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-                    options.addArguments("--no-sandbox"); // Bypass OS security model
                     return new ChromeDriver(options);
                 case "internet explorer":
                     System.setProperty("webdriver.ie.driver", PropertyProvider.getInstance().getProperty("ie.driver.path"));
